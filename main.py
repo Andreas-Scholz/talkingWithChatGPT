721862ce9c08252b73dc789d35f4adeff5c38430
Andreas Scholz <andreas.scholz@fastmail.com>
2023-04-21 22:42:08 +0200

    Added keyword functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import speech_recognition as sr
 import openai
+import re
 
 
 # OpenAI config
@@ -10,27 +11,33 @@ chatgpt_model = "gpt-3.5-turbo"  # available models via openai.Model.list()
 # Google config
 r = sr.Recognizer()
 
-# "Wolfgang, wenn gestern Montag war, was wäre dann übermorgen für ein Tag?";  <- ChatGPT 4.0 kann das, 3.5 offenbar nicht
+# "Wolfgang, wenn gestern Montag war, was wäre dann übermorgen für ein Tag?";
 audio_wolfgang_wochentag_wav = "resources/wolfgang-wochentag.wav"
 audio_wolfgang_wochentag_mp3 = "resources/wolfgang-wochentag.mp3"
 
+
 # General settings
-tts = "openai"  # the text to speech vendor to use
+stt = "openai"  # speech to text vendor to use
 audio_input_language = "de"
+keyword = "Wolfgang"  # the word used to activate chatGPT (instead of alexa) The keyword will be stripped from the beginning of the stt transcript
+keyword_stripping_pattern = '^' + keyword + ',?\s*(.*)$'  # strip the keyword from the beginning together with an existing comma and whitespace
 
 
 def main():
     transcription = transcribe()
-    print(transcription)
-    input("Press enter if you want to send this to ChatGPT... (otherwise please quit as this costs money):\n")
-    response = talk_to_chatgpt(transcription)
+    print("Transcription: " + transcription)
 
+    transcription_without_keyword = strip_keyword(transcription)
+    print("Keyword removed: " + transcription_without_keyword)
+
+    input("Press enter if you want to send this to ChatGPT... (otherwise please quit as this costs money):")
+    response = talk_to_chatgpt(transcription_without_keyword)
     print(response.content)
 
 
 def transcribe():
     transcription = ""
-    match tts:
+    match stt:
         case "openai" | "whisper":
             transcription = transcribe_with_openai(audio_wolfgang_wochentag_mp3)
         case "google":
@@ -52,6 +59,7 @@ def transcribe_with_google(audio_file):
     transcript = r.recognize_google(audio_data, language=audio_input_language)
     return transcript
 
+
 def talk_to_chatgpt(message):
     completion = openai.ChatCompletion.create(
         model=chatgpt_model,
@@ -60,6 +68,14 @@ def talk_to_chatgpt(message):
         ]
     )
     return completion.choices[0].message
+
+
+def strip_keyword(text: str):
+    try:
+        stripped_text = re.search(keyword_stripping_pattern, text).group(1)
+        return stripped_text.capitalize()  # capitalize the first letter
+    except:
+        return text
 
 
 if __name__ == '__main__':
